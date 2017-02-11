6a095933ffe329a285a06bcbc5c84294bf6110d6
karmas <karmas>
2017-02-11 19:15:36 +0000

    added for trie
    - lazy size
    - getting keys
    - longest prefix of
    - delete

--- 5-strings/testtrie.cc
+++ 5-strings/testtrie.cc
@@ -2,6 +2,7 @@
 #include "utils.h"
 #include <map>
 #include <set>
+#include <algorithm>
 #include "gtest/gtest.h"
 
 using namespace std;
@@ -17,8 +18,8 @@ TEST( trie, random_apis )
   Trie<int> trie;
 
   for (int i = 0; i < calls; i++) {
-    int randapi = rand() % 5;
-    if (randapi < 3) {
+    int randapi = rand() % 10;
+    if (randapi < 5) {
       string key;
       randomString(key, 20);
       if (debug) {
@@ -28,7 +29,7 @@ TEST( trie, random_apis )
       trie.put(key, defaultVal);
       ASSERT_TRUE(trie.contains(key));
     }
-    else {
+    else if (randapi < 8) {
       if (refst.size() != 0) {
         int randomEntry = rand() % refst.size();
         const string *key = 0;
@@ -40,12 +41,45 @@ TEST( trie, random_apis )
           printf("getting key=%s\n", key->c_str());
         }
         int trieVal = trie.get(*key);
-        ASSERT_TRUE(trieVal != 0);
+        ASSERT_EQ(defaultVal, trieVal);
       }
     }
+    else {
+      if (refst.size() == 0) continue;
+      int randomEntry = rand() % refst.size();
+      map<string, int>::iterator it = refst.begin();
+      for ( ; it != refst.end() && randomEntry > 0; ++it, --randomEntry);
+      const string key(it->first);
+      if (debug) {
+        printf("deleting key=%s\n", key.c_str());
+      }
+      refst.erase(key);
+      trie.remove(key);
+      int trieVal = trie.get(key);
+      ASSERT_EQ(0, trieVal);
+    }
+
+    EXPECT_EQ(refst.size(), trie.sizeCalc());
+    ASSERT_EQ(refst.size(), trie.size());
   }
 
+  EXPECT_EQ(refst.size(), trie.sizeCalc());
   ASSERT_EQ(refst.size(), trie.size());
+
+  vector<string> refKeys;
+  for (map<string, int>::const_iterator it = refst.begin(); it != refst.end(); ++it) {
+    refKeys.push_back(it->first);
+  }
+  sort(refKeys.begin(), refKeys.end());
+
+  vector<string> trieKeys;
+  trie.keys(trieKeys);
+  sort(trieKeys.begin(), trieKeys.end());
+
+  ASSERT_EQ(refKeys.size(), trieKeys.size());
+  for (size_t i = 0; i < refKeys.size(); ++i) {
+    ASSERT_EQ(refKeys[i], trieKeys[i]);
+  }
 }
 
 int main(int argc, char **argv)
--- 5-strings/trie.h
+++ 5-strings/trie.h
@@ -2,6 +2,7 @@
 #define TRIE_H
 
 #include <iostream>
+#include <vector>
 
 template <typename val_t>
 struct Trie {
@@ -55,6 +56,106 @@ struct Trie {
   }
 
   int size() const { return _size; }
+
+  int sizeCalc(Node *node) const {
+    if (!node) {
+      return 0;
+    }
+
+    int total = 0;
+    if (node->_val != 0) total += 1;
+
+    for (int i = 0; i < 26; i++) {
+      total += sizeCalc(node->_links[i]);
+    }
+
+    return total;
+  }
+  int sizeCalc() const {
+    return sizeCalc(_root);
+  }
+
+  void keys(std::vector<std::string> &list) const {
+    keysWithPrefix("", list);
+  }
+  void keysWithPrefix(std::string prefix,
+      std::vector<std::string> &list) const {
+    Node *node = find(_root, prefix, 0);
+    collect(node, prefix, list);
+  }
+  void collect(Node *node, std::string pre,
+      std::vector<std::string> &list) const {
+    if (!node) return;
+    if (node->_val != 0) {
+      list.push_back(pre);
+    }
+    for (int i = 0; i < 26; i++) {
+      char keylink = 'a' + i;
+      std::string newPre = pre + keylink;
+      collect(node->_links[i], newPre, list);
+    }
+  }
+
+  void keysThatMatch(std::string pattern,
+      std::vector<std::string> &list) const {
+    collect(_root, "", pattern, list);
+  }
+  void collect(Node *node, std::string pre, std::string pattern,
+      std::vector<std::string> &list) const {
+    if (!node) return;
+    if (pre.size() == pattern.size()) {
+      if (node->_val != 0) {
+        list.push_back(pre);
+      }
+      return;
+    }
+
+    char next = pattern[pre.size()];
+    for (int i = 0; i < 26; i++) {
+      char keylink = 'a' + i;
+      std::string newPre = pre + keylink;
+      if (next == '.' || next == keylink)
+        collect(node->_links[i], newPre, pattern, list);
+    }
+  }
+
+  std::string longestPrefixOf(const std::string &s) const {
+    std::string key;
+    int len = search(_root, s, 0, 0);
+    return key.substr(0, len);
+  }
+  int search(Node *node, const std::string &s, int sc, int len) const {
+    if (!node) return len;
+    if (node->_val != 0) {
+      len = sc;
+    }
+    if (sc == s.size()) return len;
+
+    char keylink = s[sc] - 'a';
+    return search(node->_links[keylink], s, sc + 1, len);
+  }
+
+  void remove(const std::string &key) {
+    _root = remove(_root, key, 0);
+  }
+  Node *remove(Node *node, const std::string &key, int kc) {
+    if (!node) return node;
+    // found key
+    if (kc == key.size()) {
+      if (node->_val) --_size;
+      node->_val = 0;
+    }
+    else {
+      int keyIndex = key[kc] - 'a';
+      node->_links[keyIndex] = remove(node->_links[keyIndex], key, kc + 1);
+    }
+
+    if (node->_val != 0) return node;
+    for (int i = 0; i < 26; i++)
+      if (node->_links['a' + i]) return node;
+    delete node;
+    return 0;
+  }
 
   int _size;
   Node *_root;
